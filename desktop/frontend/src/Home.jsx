b90d5f27e4d82a1e58ebe40dce04bf599f0886ce
BlueSkyCaps <linsongxian@outlook.com>
2024-01-14 17:17:50 +0800

    桌面端：基本布局

---
+++ desktop/frontend/src/Home.jsx
@@ -0,0 +1,33 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome1 to my website!</h1>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      {/* Add more components and content here */}
+    </div>
+  );
+}
+
+export default Home
---
+++ desktop/frontend/src/Setting.jsx
@@ -0,0 +1,16 @@
+function Setting() {
+  return (
+    <div>
+      <h1>Setting!</h1>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      <p>This is a sample paragraph.</p>
+      {/* Add more components and content here */}
+    </div>
+  );
+}
+
+export default Setting
