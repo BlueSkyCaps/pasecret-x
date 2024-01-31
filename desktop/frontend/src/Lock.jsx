b684c7d343753e2b5c67c9348945ab3e35803414
BlueSkyCaps <linsongxian@outlook.com>
2024-01-31 22:33:42 +0800

    桌面端：前端解锁成功显示首页

--- desktop/frontend/src/Lock.jsx
+++ desktop/frontend/src/Lock.jsx
@@ -1,5 +1,6 @@
 import {Button, Col, Progress, Row, Space} from "antd";
 import {useState} from "react";
+import App from "./App";
 let addClickedCounter = 0;
 let addClickedStr = "";
 function Lock() {
@@ -10,6 +11,9 @@ function Lock() {
         if (addClickedCounter>=4){
             if (addClickedStr==="1234"){
                 alert(addClickedStr)
+                setPass(true)
+                return
+                // end ignore all logic
             }else {
                 alert("密码错误")
             }
@@ -19,46 +23,47 @@ function Lock() {
         }
     }
     const [percent, setPercent] = useState(0);
+    const [pass, setPass] = useState(false);
     return (
-        <div>
-            <h4 style={{color:"gray"}}>请解锁</h4>
-            <Space>
-                <Progress strokeColor="#7ac5cd" showInfo={false} steps={4} percent={percent} size={25} />
-            </Space>
-            <Row  className="numberPadding">
-                <Col span={24}>
-                    <Space size="large">
-                        <Button onClick={()=>numberBtnInputHandler(1)} style={{width:"70px",height:"70px"}} shape="circle"><span style={{fontSize:"40px", color:"gray"}}>1</span></Button>
-                        <Button onClick={()=>numberBtnInputHandler(2)} style={{width:"70px",height:"70px"}} shape="circle"><span style={{fontSize:"40px", color:"gray"}}>2</span></Button>
-                        <Button onClick={()=>numberBtnInputHandler(3)} style={{width:"70px",height:"70px"}} shape="circle"><span style={{fontSize:"40px", color:"gray"}}>3</span></Button>
-                    </Space>
-                </Col>
-            </Row>
-            <Row  className="numberPadding">
-                <Col span={24}>
-                    <Space size="large">
-                        <Button onClick={()=>numberBtnInputHandler(4)} style={{width:"70px",height:"70px"}} shape="circle"><span style={{fontSize:"40px", color:"gray"}}>4</span></Button>
-                        <Button onClick={()=>numberBtnInputHandler(5)} style={{width:"70px",height:"70px"}} shape="circle"><span style={{fontSize:"40px", color:"gray"}}>5</span></Button>
-                        <Button onClick={()=>numberBtnInputHandler(6)} style={{width:"70px",height:"70px"}} shape="circle"><span style={{fontSize:"40px", color:"gray"}}>6</span></Button>
-                    </Space>
-                </Col>
-            </Row>
-            <Row  className="numberPadding">
-                <Col span={24}>
-                    <Space size="large">
-                        <Button onClick={()=>numberBtnInputHandler(7)} style={{width:"70px",height:"70px"}} shape="circle"><span style={{fontSize:"40px", color:"gray"}}>7</span></Button>
-                        <Button onClick={()=>numberBtnInputHandler(8)} style={{width:"70px",height:"70px"}} shape="circle"><span style={{fontSize:"40px", color:"gray"}}>8</span></Button>
-                        <Button onClick={()=>numberBtnInputHandler(9)} style={{width:"70px",height:"70px"}} shape="circle"><span style={{fontSize:"40px", color:"gray"}}>9</span></Button>
-                    </Space>
-                </Col>
-            </Row>
-            <Row>
-                <Col span={24}>
-                    <Button onClick={()=>numberBtnInputHandler(0)} style={{width:"70px",height:"70px"}} shape="circle"><span style={{fontSize:"40px", color:"gray"}}>0</span></Button>
-                </Col>
-            </Row>
-        </div>
-
+            pass ? <App/>:
+            <div>
+                <h4 style={{color:"gray"}}>请解锁</h4>
+                <Space>
+                    <Progress strokeColor="#7ac5cd" showInfo={false} steps={4} percent={percent} size={25} />
+                </Space>
+                <Row  className="numberPadding">
+                    <Col span={24}>
+                        <Space size="large">
+                            <Button onClick={()=>numberBtnInputHandler(1)} style={{width:"70px",height:"70px"}} shape="circle"><span style={{fontSize:"40px", color:"gray"}}>1</span></Button>
+                            <Button onClick={()=>numberBtnInputHandler(2)} style={{width:"70px",height:"70px"}} shape="circle"><span style={{fontSize:"40px", color:"gray"}}>2</span></Button>
+                            <Button onClick={()=>numberBtnInputHandler(3)} style={{width:"70px",height:"70px"}} shape="circle"><span style={{fontSize:"40px", color:"gray"}}>3</span></Button>
+                        </Space>
+                    </Col>
+                </Row>
+                <Row  className="numberPadding">
+                    <Col span={24}>
+                        <Space size="large">
+                            <Button onClick={()=>numberBtnInputHandler(4)} style={{width:"70px",height:"70px"}} shape="circle"><span style={{fontSize:"40px", color:"gray"}}>4</span></Button>
+                            <Button onClick={()=>numberBtnInputHandler(5)} style={{width:"70px",height:"70px"}} shape="circle"><span style={{fontSize:"40px", color:"gray"}}>5</span></Button>
+                            <Button onClick={()=>numberBtnInputHandler(6)} style={{width:"70px",height:"70px"}} shape="circle"><span style={{fontSize:"40px", color:"gray"}}>6</span></Button>
+                        </Space>
+                    </Col>
+                </Row>
+                <Row  className="numberPadding">
+                    <Col span={24}>
+                        <Space size="large">
+                            <Button onClick={()=>numberBtnInputHandler(7)} style={{width:"70px",height:"70px"}} shape="circle"><span style={{fontSize:"40px", color:"gray"}}>7</span></Button>
+                            <Button onClick={()=>numberBtnInputHandler(8)} style={{width:"70px",height:"70px"}} shape="circle"><span style={{fontSize:"40px", color:"gray"}}>8</span></Button>
+                            <Button onClick={()=>numberBtnInputHandler(9)} style={{width:"70px",height:"70px"}} shape="circle"><span style={{fontSize:"40px", color:"gray"}}>9</span></Button>
+                        </Space>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={24}>
+                        <Button onClick={()=>numberBtnInputHandler(0)} style={{width:"70px",height:"70px"}} shape="circle"><span style={{fontSize:"40px", color:"gray"}}>0</span></Button>
+                    </Col>
+                </Row>
+            </div>
     );
 }
 
