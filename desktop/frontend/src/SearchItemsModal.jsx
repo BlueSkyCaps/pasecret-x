adfcc60c26dabcad9effc7fac5c7fc435a5f53bd
BlueSkyCaps <linsongxian@outlook.com>
2024-02-06 00:43:28 +0800

    桌面端：搜索颜色

--- desktop/frontend/src/SearchItemsModal.jsx
+++ desktop/frontend/src/SearchItemsModal.jsx
@@ -1,7 +1,8 @@
-import {Modal, Space, Table} from "antd";
+import {Button, message, Modal, Space, Table} from "antd";
 import {useTranslation} from "react-i18next";
 import {useContext} from "react";
 import {PassDtoContext} from "./Core";
+import {CopyFilled} from "@ant-design/icons";
 
 const SearchItemsModal = ({isModalOpen,setIsModalOpen,modalDisplayData}) => {
     const {t} = useTranslation()
@@ -26,21 +27,40 @@ const SearchItemsModal = ({isModalOpen,setIsModalOpen,modalDisplayData}) => {
         d.password.toLowerCase().includes(tmpValue)||
         d.account_name.toLowerCase().includes(tmpValue)
     )
-    // 因为data数组没有key键，而react渲染列表需要指定每个元素有key来标识唯一，所以在基础上追加key键值对仅此而已
+    // 因为data数组没有key键，而react渲染列表需要指定每个元素有key来标识唯一，所以在基础上追加key键值对
+    // 以及追加所属的归类夹名称
     dataSource = tmpDataSource.map((d,i)=>{
         return {
             ...d,
-            key:i
+            key:i,
+            _category_name:PassDtoReceived.loadedItems.category.find(c=>c.id===d.category_id).name
         }
     })
 
     const columns = [
+        // 密码项所属归类夹名称
+        {
+            title: t("SearchDataResultHeaderVDataCategoryName"),
+            dataIndex: '_category_name',
+            key: '_category_name',
+            width: 120,
+            ellipsis: true,
+            // 数据单元格的渲染函数，参数分别为当前行的值，当前行数据，行索引
+            render: (title, item) => (
+                <span style={{color:"saddlebrown"}}>{title}</span>
+            ),
+        },
+        // 密码项名称
         {
             title: t("SearchDataResultHeaderVDataName"),
             dataIndex: 'name',
             key: 'name',
             width: 120,
             ellipsis: true,
+            // 数据单元格的渲染函数，参数分别为当前行的值，当前行数据，行索引
+            render: (title, item) => (
+                <span style={{color:"green"}}>{title}</span>
+            ),
         },
         {
             title: t("SearchDataResultHeaderVDataAccountName"),
@@ -51,7 +71,7 @@ const SearchItemsModal = ({isModalOpen,setIsModalOpen,modalDisplayData}) => {
         },
         {
             title: t("SearchDataResultHeaderVDataPassword"),
-            dataIndex: t(""),
+            dataIndex: 'password',
             key: 'password',
             width: 100,
             ellipsis: true,
@@ -73,19 +93,36 @@ const SearchItemsModal = ({isModalOpen,setIsModalOpen,modalDisplayData}) => {
         {
             title: '-',
             key: 'action',
-            render: (_, record) => (
-                <Space size="middle">
-                    <a onClick={()=>{alert( record.name)}}>Delete</a>
-                </Space>
+            render: (_, item) => (
+                <Button onClick={()=>{copyItemClick(item.id, item.category_id)}} icon={<CopyFilled />}/>
             ),
         },
     ];
+
+    // 点击了复制按钮 复制该条密码项
+    async function copyItemClick(itemId, category_id) {
+        // 根据id找到密码项
+        let copyItem = dataSource.find(item => item.id === itemId && item.category_id === category_id);
+        let copyStr = t("copyStr", {
+            "name": copyItem.name,
+            "remark": copyItem.remark,
+            "site": copyItem.site,
+            "password": copyItem.password,
+            "account_name": copyItem.account_name
+        })
+        try {
+            await navigator.clipboard.writeText(copyStr);
+            message.success(t("copyClipboardDone"));
+        } catch (err) {
+            message.error(t("dialogShowErrorTitle") + err);
+        }
+    }
 
     return (
         <>
             <Modal cancelButtonProps={{ style: { display: 'none' } }}
                    okButtonProps={{ style: { backgroundColor:"green" } }}
-                   width={700}
+                   width={820}
                    okText={t("SearchWindowOkText")} centered={true} maskClosable={false} closeIcon={null}
                    title={t("SearchWindowTitle")} open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
                 <Table dataSource={dataSource} columns={columns}
