a74fa75bcdc66c4feca8c8a0277722c5d7b66c75
BlueSkyCaps <linsongxian@outlook.com>
2024-06-30 15:47:40 +0800

    remove add category name value

--- mobile/app/(category-item)/add.tsx
+++ mobile/app/(category-item)/add.tsx
@@ -11,7 +11,7 @@ export default function AddCategoryScreen() {
   console.log(cid)
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>item add</Text>
+      <Text style={styles.title}>item add {cid}</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
